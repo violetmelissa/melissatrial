969b20cf94a4ed0a3ca9eb46a9a3325faf0c6fdd
Emma Paris <melissa8fon@gmail.com>
2020-05-08 17:23:55 +0530

    sketch is added

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    ground2 = new Ground(150,300,300,200);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -59,4 +59,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    ground2.display();
+    
 }
